104c5e921ddb7da2a6a64a8e94cbb43855df08b8
Erich Gubler <erichdongubler@gmail.com>
2023-10-09 17:36:48 -0400

    feat!: finish impl. of conditional values

--- src/lib.rs
+++ src/lib.rs
@@ -170,7 +170,7 @@ pub mod metadata {
         Unconditional(&'a str),
         Conditional {
             conditions: Vec<(Condition<'a>, &'a str)>,
-            fallback: &'a str,
+            fallback: Option<&'a str>,
         },
     }
 
@@ -255,57 +255,7 @@ pub mod metadata {
             Comment,
         }
 
-        let property = |indentation: u8| {
-            let unconditional_value = any()
-                .and_is(newline().not())
-                .repeated()
-                .slice()
-                .then_ignore(newline());
-            let conditional_rule = group((
-                indent(
-                    indentation
-                        .checked_add(1)
-                        .expect("unexpectedly high indentation level"),
-                ),
-                keyword("if"),
-                just(' '),
-            ))
-            .ignore_then(
-                // TODO: actual expression tree
-                any()
-                    .and_is(choice((newline(), just(":").to(()))).not())
-                    .repeated()
-                    .slice()
-                    .map(Condition)
-                    .then_ignore(group((just(':'), inline_whitespace())))
-                    .then(
-                        any()
-                            .and_is(choice((newline(), just(':').to(()))).not())
-                            .repeated()
-                            .slice(),
-                    ),
-            );
-            let conditional_fallback = keyword("if")
-                .not()
-                .ignore_then(any().and_is(newline().not()))
-                .slice();
-
-            indent(indentation)
-                .ignore_then(ident())
-                .then_ignore(just(':'))
-                .then_ignore(inline_whitespace())
-                .then(choice((
-                    unconditional_value.map(PropertyValue::Unconditional),
-                    newline()
-                        .ignore_then(conditional_rule.repeated().collect())
-                        .then(conditional_fallback)
-                        .map(|(conditions, fallback)| PropertyValue::Conditional {
-                            conditions,
-                            fallback,
-                        }),
-                )))
-        };
-        let items = choice((
+        let subtest = || {
             section_name(1)
                 .then_ignore(newline())
                 .labelled("subtest section header")
@@ -319,9 +269,15 @@ pub mod metadata {
                     name: subtest_name,
                     properties: properties.into_iter().collect(),
                 })
-                .labelled("subtest"),
-            property(1).map(|(key, value)| Item::Property { key, value }),
-            newline().to(Item::Newline),
+                .labelled("subtest")
+        };
+
+        let items = choice((
+            subtest(),
+            property(1)
+                .labelled("test property")
+                .map(|(key, value)| Item::Property { key, value }),
+            newline().labelled("empty line").to(Item::Newline),
             comment().to(Item::Comment),
         ))
         .repeated()
@@ -402,6 +358,197 @@ pub mod metadata {
             }
             "###
         );
+    }
+
+    fn property<'a>(
+        indentation: u8,
+    ) -> impl Parser<'a, &'a str, (&'a str, PropertyValue<'a>), ParseError<'a>> {
+        let conditional_indent_level = indentation
+            .checked_add(1)
+            .expect("unexpectedly high indentation level");
+
+        let property_value = || {
+            choice((
+                unconditional_value().map(PropertyValue::Unconditional),
+                conditional_value(conditional_indent_level).map(|(conditions, fallback)| {
+                    PropertyValue::Conditional {
+                        conditions,
+                        fallback,
+                    }
+                }),
+            ))
+            .labelled("property value")
+        };
+
+        property_key(indentation)
+            .then_ignore(group((just(':'), inline_whitespace())))
+            .then(property_value())
+    }
+
+    fn property_key<'a>(indentation: u8) -> impl Parser<'a, &'a str, &'a str, ParseError<'a>> {
+        indent(indentation)
+            .ignore_then(ident())
+            .labelled("property key")
+    }
+
+    fn unconditional_value<'a>() -> impl Parser<'a, &'a str, &'a str, ParseError<'a>> {
+        any()
+            .and_is(newline().not())
+            .repeated()
+            .at_least(1)
+            .slice()
+            .then_ignore(newline())
+            .labelled("unconditional value")
+    }
+
+    fn conditional_rule<'a>(
+        indentation: u8,
+    ) -> impl Parser<'a, &'a str, (Condition<'a>, &'a str), ParseError<'a>> {
+        group((indent(indentation), keyword("if"), just(' ')))
+            .ignore_then(
+                any()
+                    .and_is(choice((newline(), just(":").to(()))).not())
+                    .repeated()
+                    .slice()
+                    .map(Condition)
+                    .then_ignore(group((just(':'), inline_whitespace())))
+                    .then(
+                        any()
+                            .and_is(choice((newline(), just(':').to(()))).not())
+                            .repeated()
+                            .slice(),
+                    ),
+            )
+            .then_ignore(newline().or(end()))
+            .labelled("conditional value rule")
+    }
+
+    fn conditional_fallback<'a>(
+        indentation: u8,
+    ) -> impl Parser<'a, &'a str, &'a str, ParseError<'a>> {
+        indent(indentation)
+            .ignore_then(
+                keyword("if")
+                    .not()
+                    .ignore_then(any().and_is(newline().not()).repeated())
+                    .slice(),
+            )
+            .then_ignore(newline().or(end()))
+            .labelled("conditional value fallback")
+    }
+
+    #[test]
+    fn test_conditional_fallback() {
+        assert_debug_snapshot!(conditional_fallback(0).parse("[PASS, FAIL]"), @r###"
+            ParseResult {
+                output: Some(
+                    "[PASS, FAIL]",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(0).parse(r#""okgo""#), @r###"
+            ParseResult {
+                output: Some(
+                    "\"okgo\"",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(0).parse(""), @r###"
+            ParseResult {
+                output: Some(
+                    "",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(1).parse(""), @r###"
+            ParseResult {
+                output: None,
+                errs: [
+                    found end of input at 0..0 expected '' '',
+                ],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(1).parse("  "), @r###"
+            ParseResult {
+                output: Some(
+                    "",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(1).parse("  @False"), @r###"
+            ParseResult {
+                output: Some(
+                    "@False",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(1).parse("    @False"), @r###"
+            ParseResult {
+                output: None,
+                errs: [
+                    found '' '' at 2..3 expected something else,
+                ],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(3).parse(""), @r###"
+            ParseResult {
+                output: None,
+                errs: [
+                    found end of input at 0..0 expected '' '',
+                ],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(3).parse("      "), @r###"
+            ParseResult {
+                output: Some(
+                    "",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(3).parse("      @True"), @r###"
+            ParseResult {
+                output: Some(
+                    "@True",
+                ),
+                errs: [],
+            }
+            "###);
+        assert_debug_snapshot!(conditional_fallback(3).parse("        @True"), @r###"
+            ParseResult {
+                output: None,
+                errs: [
+                    found '' '' at 6..7 expected something else,
+                ],
+            }
+            "###);
+    }
+
+    fn conditional_value<'a>(
+        indentation: u8,
+    ) -> impl Parser<'a, &'a str, (Vec<(Condition<'a>, &'a str)>, Option<&'a str>), ParseError<'a>>
+    {
+        newline()
+            .ignore_then(conditional_rule(indentation).repeated().collect::<Vec<_>>())
+            .then(conditional_fallback(indentation).or_not())
+            .validate(|(conditions, fallback), span, emitter| {
+                if conditions.is_empty() && fallback.is_none() {
+                    emitter.emit(Rich::custom(
+                        span,
+                        concat!(
+                            "this conditional property value has no conditional ",
+                            "rules or fallback specified",
+                        ),
+                    ));
+                }
+                (conditions, fallback)
+            })
+            .labelled("conditional value")
     }
 
     fn indent<'a>(level: u8) -> impl Parser<'a, &'a str, (), ParseError<'a>> {
