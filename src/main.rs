4fabd4dbd0988b0470fe1e66d95f61e819392139
Erich Gubler <erichdongubler@gmail.com>
2023-10-09 16:15:03 -0400

    refactor(main): use `log` for file read errors, try all before bailing

--- src/main.rs
+++ src/main.rs
@@ -42,34 +42,47 @@ fn run(cli: Cli) -> ExitCode {
     } = cli;
     match subcommand {
         Subcommand::DumpTestExps => {
-            let raw_test_exps_by_path = (1..=51)
-                .into_iter()
-                .map(|chunk| {
-                    let wpt_expectation_file_path = {
-                        let chunk = chunk.to_string();
-                        path!(
-                            &gecko_checkout
-                                | "testing"
-                                | "web-platform"
-                                | "mozilla"
-                                | "meta"
-                                | "webgpu"
-                                | "chunked"
-                                | &chunk
-                                | "cts.https.html.ini"
-                        )
-                    };
-                    eprintln!("{}", wpt_expectation_file_path.display());
-                    let contents = fs::read_to_string(&wpt_expectation_file_path).unwrap();
-                    (
-                        wpt_expectation_file_path
-                            .strip_prefix(&gecko_checkout)
-                            .unwrap()
-                            .to_owned(),
-                        contents,
-                    )
-                })
-                .collect::<IndexMap<_, _>>();
+            let raw_test_exps_by_path = {
+                let mut found_read_err = false;
+                let data = (1..=51)
+                    .into_iter()
+                    .filter_map(|chunk| {
+                        let wpt_expectation_file_path = {
+                            let chunk = chunk.to_string();
+                            path!(
+                                &gecko_checkout
+                                    | "testing"
+                                    | "web-platform"
+                                    | "mozilla"
+                                    | "meta"
+                                    | "webgpu"
+                                    | "chunked"
+                                    | &chunk
+                                    | "cts.https.html.ini"
+                            )
+                        };
+                        log::debug!("reading from {}…", wpt_expectation_file_path.display());
+                        match fs::read_to_string(&wpt_expectation_file_path) {
+                            Err(e) => {
+                                log::error!("failed to read {wpt_expectation_file_path:?}: {e}");
+                                found_read_err = true;
+                                None
+                            }
+                            Ok(contents) => Some((
+                                wpt_expectation_file_path
+                                    .strip_prefix(&gecko_checkout)
+                                    .unwrap()
+                                    .to_owned(),
+                                contents,
+                            )),
+                        }
+                    })
+                    .collect::<IndexMap<_, _>>();
+                if found_read_err {
+                    return ExitCode::FAILURE;
+                }
+                data
+            };
             #[derive(Debug)]
             struct TestExpEntry<'a> {
                 orig_path: &'a Path,
