004c408ef90d98d87ca6dff7657ef4b5396fe343
Erich Gubler <erichdongubler@gmail.com>
2023-10-27 15:56:41 -0400

    move: promote `NormalizedExpectationPropertyValue::from_full` to `shared`

--- moz-webgpu-cts/src/metadata.rs
+++ moz-webgpu-cts/src/metadata.rs
@@ -390,68 +390,6 @@ where
     }
 }
 
-impl<Out> NormalizedExpectationPropertyValue<Out>
-where
-    Out: Clone + Default + EnumSetType + Eq + PartialEq,
-{
-    fn from_full(
-        outcomes: BTreeMap<Platform, BTreeMap<BuildProfile, Expectation<Out>>>,
-    ) -> Option<Self> {
-        if outcomes.is_empty() {
-            return None;
-        }
-
-        fn normalize<K, V, T, F>(
-            mut map: BTreeMap<K, V>,
-            mut f: F,
-        ) -> MaybeCollapsed<T, BTreeMap<K, T>>
-        where
-            F: FnMut(V) -> T,
-            K: IntoEnumIterator + Ord,
-            V: Default,
-            T: Clone + Default + Eq + PartialEq,
-        {
-            fn skip_default<K, V, I>(iter: I) -> impl Iterator<Item = (K, V)>
-            where
-                I: IntoIterator<Item = (K, V)>,
-                V: Default + Eq + PartialEq,
-            {
-                iter.into_iter().filter(|(_k, v)| v != &Default::default())
-            }
-
-            let mut iter = K::iter().map(|k| {
-                let v = map.remove(&k).unwrap_or_default();
-                (k, f(v))
-            });
-
-            let (first_key, first_t) = iter.next().unwrap();
-
-            let mut inconsistency_found = false;
-            let mut expanded = BTreeMap::default();
-            for (k, t) in iter.by_ref() {
-                if t == first_t {
-                    expanded.extend(skip_default([(k, t)]));
-                } else {
-                    inconsistency_found = true;
-                    expanded.extend(skip_default([(k, t)].into_iter().chain(iter)));
-                    break;
-                }
-            }
-            if inconsistency_found {
-                expanded.extend(skip_default([(first_key, first_t)]));
-                MaybeCollapsed::Expanded(expanded)
-            } else {
-                MaybeCollapsed::Collapsed(first_t)
-            }
-        }
-
-        Some(NormalizedExpectationPropertyValue(normalize(
-            outcomes,
-            |by_build_profile| normalize(by_build_profile, std::convert::identity),
-        )))
-    }
-}
-
 #[derive(Clone, Debug, Default)]
 pub struct Applicability {
     pub platform: Option<Platform>,
--- moz-webgpu-cts/src/shared.rs
+++ moz-webgpu-cts/src/shared.rs
@@ -1,6 +1,7 @@
 use std::collections::BTreeMap;
 
 use enumset::{EnumSet, EnumSetType};
+use strum::IntoEnumIterator;
 
 use crate::metadata::{BuildProfile, Platform};
 
@@ -80,5 +81,65 @@ where
     pub fn into_inner(self) -> NormalizedExpectationPropertyValueData<Out> {
         let Self(inner) = self;
         inner
+    }
+
+    pub(crate) fn from_full(
+        outcomes: BTreeMap<Platform, BTreeMap<BuildProfile, Expectation<Out>>>,
+    ) -> Option<Self>
+    where
+        Out: Default,
+    {
+        if outcomes.is_empty() {
+            return None;
+        }
+
+        fn normalize<K, V, T, F>(
+            mut map: BTreeMap<K, V>,
+            mut f: F,
+        ) -> MaybeCollapsed<T, BTreeMap<K, T>>
+        where
+            F: FnMut(V) -> T,
+            K: IntoEnumIterator + Ord,
+            V: Default,
+            T: Clone + Default + Eq + PartialEq,
+        {
+            fn skip_default<K, V, I>(iter: I) -> impl Iterator<Item = (K, V)>
+            where
+                I: IntoIterator<Item = (K, V)>,
+                V: Default + Eq + PartialEq,
+            {
+                iter.into_iter().filter(|(_k, v)| v != &Default::default())
+            }
+
+            let mut iter = K::iter().map(|k| {
+                let v = map.remove(&k).unwrap_or_default();
+                (k, f(v))
+            });
+
+            let (first_key, first_t) = iter.next().unwrap();
+
+            let mut inconsistency_found = false;
+            let mut expanded = BTreeMap::default();
+            for (k, t) in iter.by_ref() {
+                if t == first_t {
+                    expanded.extend(skip_default([(k, t)]));
+                } else {
+                    inconsistency_found = true;
+                    expanded.extend(skip_default([(k, t)].into_iter().chain(iter)));
+                    break;
+                }
+            }
+            if inconsistency_found {
+                expanded.extend(skip_default([(first_key, first_t)]));
+                MaybeCollapsed::Expanded(expanded)
+            } else {
+                MaybeCollapsed::Collapsed(first_t)
+            }
+        }
+
+        Some(NormalizedExpectationPropertyValue(normalize(
+            outcomes,
+            |by_build_profile| normalize(by_build_profile, std::convert::identity),
+        )))
     }
 }
