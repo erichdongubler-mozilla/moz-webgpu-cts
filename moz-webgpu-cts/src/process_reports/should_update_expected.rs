8dcde14c0812857696879b9b4d5fcd712d0ea9e0
Erich Gubler <erichdongubler@gmail.com>
2024-08-01 13:19:35 -0400

    refactor(update_expected): hoist logic for `expected` filtering to callers of `process_reports`

---
+++ moz-webgpu-cts/src/process_reports/should_update_expected.rs
@@ -0,0 +1,62 @@
+use std::fmt::Debug;
+
+use enumset::EnumSet;
+
+use crate::wpt::metadata::{
+    properties::{Expected, NonNormalizedPropertyValue},
+    BuildProfile, ImplementationStatus, Platform, SubtestOutcome, TestOutcome, TestProps,
+};
+
+pub(crate) trait ShouldUpdateExpected: Debug {
+    fn test(
+        &mut self,
+        meta_props: &TestProps<TestOutcome>,
+        reported: &NonNormalizedPropertyValue<Expected<TestOutcome>>,
+        key: (Platform, BuildProfile),
+    ) -> bool;
+    fn subtest(
+        &mut self,
+        meta_props: &TestProps<SubtestOutcome>,
+        reported: &NonNormalizedPropertyValue<Expected<SubtestOutcome>>,
+        parent_meta_props: &TestProps<TestOutcome>,
+        key: (Platform, BuildProfile),
+    ) -> bool;
+}
+
+#[derive(Debug)]
+pub(crate) struct ImplementationStatusFilter {
+    pub allowed: EnumSet<ImplementationStatus>,
+}
+
+impl ImplementationStatusFilter {
+    fn is_allowed(&self, implementation_status: ImplementationStatus) -> bool {
+        let Self { allowed } = self;
+        allowed.contains(implementation_status)
+    }
+}
+
+impl ShouldUpdateExpected for ImplementationStatusFilter {
+    fn test(
+        &mut self,
+        meta_props: &TestProps<TestOutcome>,
+        _reported: &NonNormalizedPropertyValue<Expected<TestOutcome>>,
+        key: (Platform, BuildProfile),
+    ) -> bool {
+        let status = meta_props.implementation_status.unwrap_or_default()[key];
+        self.is_allowed(status)
+    }
+
+    fn subtest(
+        &mut self,
+        meta_props: &TestProps<SubtestOutcome>,
+        _reported: &NonNormalizedPropertyValue<Expected<SubtestOutcome>>,
+        parent_meta_props: &TestProps<TestOutcome>,
+        key: (Platform, BuildProfile),
+    ) -> bool {
+        let status = meta_props
+            .implementation_status
+            .or(parent_meta_props.implementation_status)
+            .unwrap_or_default()[key];
+        self.is_allowed(status)
+    }
+}
