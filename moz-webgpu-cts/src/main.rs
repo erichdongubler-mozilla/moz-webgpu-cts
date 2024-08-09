30c568489d494e04e1a877bdcb2c5861b4a8f4f1
Erich Gubler <erichdongubler@gmail.com>
2024-08-09 08:31:10 -0400

    build: upgrade `miette` 5.1.0 → 7.2.0, `wax` to upstream-ish

--- moz-webgpu-cts/src/main.rs
+++ moz-webgpu-cts/src/main.rs
@@ -45,7 +45,7 @@ use process_reports::{
     should_update_expected::{self, ShouldUpdateExpected},
     ProcessReportsArgs,
 };
-use wax::Glob;
+use wax::{walk::Entry as _, Glob};
 use whippit::{
     metadata::SectionHeader,
     reexport::chumsky::{self, prelude::Rich},
@@ -1168,7 +1168,7 @@ fn render_metadata_parse_errors<'a>(
         #[label]
         span: SourceSpan,
         #[source_code]
-        source_code: NamedSource,
+        source_code: NamedSource<Arc<String>>,
         inner: Rich<'static, char>,
     }
     let source_code = file_contents.clone();
